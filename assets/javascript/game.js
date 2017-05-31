83f919a951c61e3caf3c61f7f351b537184359ef
Andy Medina <Andy Medina>
2017-05-30 18:45:25 -0600

    yes

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+  <script type="text/javascript">
+
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+
+
+
+
+
+  </script>
